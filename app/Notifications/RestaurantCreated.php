99f02d6287e46f4ee3f5341fdc82b890949a62af
Ilyas Sarsenbaev <ilyastuit@gmail.com>
2020-12-04 12:42:48 +0500

    Created notifications

---
+++ app/Notifications/RestaurantCreated.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+
+class RestaurantCreated extends Notification
+{
+    use Queueable;
+
+    protected $password;
+    protected $restaurant;
+    protected $user;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct($password,$restaurant,$user)
+    {
+        $this->password = $password;
+        $this->restaurant = $restaurant;
+        $this->user = $user;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}

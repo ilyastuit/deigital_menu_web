d64a41245d8fa6d7406cce9f22735832ff513d19
Ilyas Sarsenbaev <ilyastuit@gmail.com>
2020-12-05 10:37:47 +0500

    updated configs

---
+++ config/money.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+    'USD' => [
+        'name'                => 'US Dollar',
+        'code'                => 840,
+        'precision'           => 2,
+        'subunit'             => 100,
+        'symbol'              => '$',
+        'symbol_first'        => true,
+        'decimal_mark'        => '.',
+        'thousands_separator' => ',',
+    ],
+
+    'UZS' => [
+        'name'                => 'Uzbekistan Sum',
+        'code'                => 860,
+        'precision'           => 2,
+        'subunit'             => 100,
+        'symbol'              => null,
+        'symbol_first'        => false,
+        'decimal_mark'        => '.',
+        'thousands_separator' => ',',
+    ],
+];

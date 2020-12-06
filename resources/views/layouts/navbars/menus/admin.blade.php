dc1cd29adfa9c02f82fb60a8474f304ae531af0a
Ilyas Sarsenbaev <ilyastuit@gmail.com>
2020-12-06 13:04:58 +0500

    edited layout

--- resources/views/layouts/navbars/menus/admin.blade.php
+++ resources/views/layouts/navbars/menus/admin.blade.php
@@ -4,4 +4,9 @@
             <i class="ni ni-shop text-info"></i> {{ __('Restaurants') }}
         </a>
     </li>
+     <li class="nav-item">
+        <a class="nav-link" href="{{ route('feedbacks') }}">
+            <i class="ni ni-shop text-info"></i> {{ __('Feedbacks') }}
+        </a>
+    </li>
 </ul>
